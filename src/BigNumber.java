88b14a7d1777e4650e8628c794ae422e997d1e6d
James Blake <jamesablake314@gmail.com>
2023-09-23 11:22:04 -0400

    Add basic tests, make Add/Sub work

--- src/BigNumber.java
+++ src/BigNumber.java
@@ -1,183 +1,223 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Objects;
 
 public final class BigNumber {
-    private final boolean negative;
-    private final ArrayList<Integer> numbers;
-    public BigNumber(String text) {
-        numbers = new ArrayList<>();
+	private final boolean negative;
 
-        char c = text.charAt(0);
-        if(c == '-')
-            negative = true;
-        else {
-            negative = false;
-            numbers.add(Integer.parseInt(Character.toString(c)));
-        }
+	/**
+	 * A list of numbers that make up the larger number. They are ordered from
+	 * least significant to most.
+	 * */
+	private final ArrayList<Integer> numbers;
 
-        for (int i = 1; i < text.length(); i++) {
-            c = text.charAt(text.length() - i);
-            numbers.add(Integer.parseInt(Character.toString(c)));
-        }
-    }
+	/**
+	 * Creates a BigNumber from a string of integers, optionally proceeded
+	 * by a minus sign for negatives.
+	 * @param text The string to parse into the BigNumber.
+	 */
+	public BigNumber(String text) {
+		numbers = new ArrayList<>();
 
-    // author
-    private BigNumber(ArrayList<Integer> numbers, boolean negative) {
-        this.numbers = numbers;
-        this.negative = negative;
-    }
+		char c = text.charAt(0);
+		negative = (c == '-');
 
-    public BigNumber add(BigNumber rh) {
-        if(negative ^ rh.negative)
-            return subtract(rh.inverse());
+		for (int i = 0; i < text.length() - (negative ? 1 : 0); i++) {
+			c = text.charAt(text.length() - i - 1);
+			numbers.add(Integer.parseInt(Character.toString(c)));
+		}
+	}
 
-        ArrayList<Integer> added = new ArrayList<>();
-        boolean carry = false;
+	/**
+	 * Creates a BigNumber from preformatted data.
+	 * @param numbers The preformatted list of numbers to assign to the internal list.
+	 * @param negative If the BigNumber is negative or not.
+	 */
+	private BigNumber(ArrayList<Integer> numbers, boolean negative) {
+		this.numbers = numbers;
+		this.negative = negative;
+	}
 
-        int smallSize;
-        int bigSize;
-        ArrayList<Integer> smallList;
-        ArrayList<Integer> bigList;
-        if(numbers.size() <= rh.numbers.size()) {
-            smallSize = numbers.size();
-            smallList = numbers;
-            bigSize = rh.numbers.size();
-            bigList = rh.numbers;
-        } else {
-            smallSize = rh.numbers.size();
-            smallList = rh.numbers;
-            bigSize = numbers.size();
-            bigList = numbers;
-        }
+	/**
+	 * Adds rh and this BigNumber.
+	 * @param rh The BigNumber added to this BigNumber.
+	 * @return A new BigNumber that's the result of adding rh to this BigNumber.
+	 */
+	public BigNumber add(BigNumber rh) {
+		// If A and B don't share a sign (A + -B or -A + B) you can do subtraction
+		// instead and just invert the right hand argument (A + -B -> A - B or
+		// -A + B -> -A - -B). This means that addition can be simplified, only
+		// handling cases where A and B share a sign and therefor increase in magnitude.
+		if(negative ^ rh.negative)
+			return subtract(rh.inverse());
 
-        for (int i = 0; i < smallSize; i++) {
-            int part = bigList.get(i) + smallList.get(i) + (carry ? 1 : 0);
-            if(part >= 10) {
-                carry = true;
-                part -= 10;
-            } else {
-                carry = false;
-            }
+		ArrayList<Integer> added = new ArrayList<>();
+		int size = numbers.size();
+		int rhSize = rh.numbers.size();
 
-            added.add(part);
-        }
+		// This finds the smaller and bigger number list between the left and right
+		// hand inputs, making it easy to loop through them fully regardless of which
+		// is which.
+		boolean leftIsSmaller = size < rhSize;
 
-        for(int i = smallSize; i < bigSize; i++) {
-            int part = bigList.get(i) + (carry ? 1 : 0);
-            if(part >= 10) {
-                carry = true;
-                part -= 10;
-                added.add(part);
-            } else {
-                added.addAll(bigList.subList(i, bigSize));
-                break;
-            }
-        }
-        if(carry)
-            added.add(1);
+		int smallSize = leftIsSmaller ? size : rhSize;
+		int bigSize = leftIsSmaller ? rhSize : size;
 
-        return new BigNumber(added, negative);
-    }
+		ArrayList<Integer> smallList = leftIsSmaller ? numbers : rh.numbers;
+		ArrayList<Integer> bigList = leftIsSmaller ? rh.numbers : numbers;
 
-    // 1000 - 1
-    public BigNumber subtract(BigNumber rh) {
-        if(negative ^ rh.negative)
-            return add(rh.inverse());
+		// The first loop adds the smaller number parts to the bigger number parts,
+		// carrying as needed until the smaller one runs out. If there is still a carry,
+		// after the first loop ends, the second loop continues with just the larger number
+		// until there is no longer a carry or the bigger number parts run out.
+		boolean carry = false;
+		// First loop
+		for (int i = 0; i < smallSize; i++) {
+			int part = bigList.get(i) + smallList.get(i) + (carry ? 1 : 0);
+			if(part >= 10) {
+				carry = true;
+				part -= 10;
+			} else {
+				carry = false;
+			}
 
-        ArrayList<Integer> subtracted = new ArrayList<>();
-        boolean carry = false;
-        int size = numbers.size();
-        int rhSize = rh.numbers.size();
+			added.add(part);
+		}
+		// Second loop
+		for(int i = smallSize; i < bigSize; i++) {
+			int part = bigList.get(i) + (carry ? 1 : 0);
+			if(part >= 10) {
+				carry = true;
+				part -= 10;
+				added.add(part);
+			} else {
+				carry = false;
+				added.add(part);
+				added.addAll(bigList.subList(i + 1, bigSize));
+				break;
+			}
+		}
+		// End loops
 
-        // Order matters so we need to record if we need to inverse the result.
-        boolean mustInverse = false;
-        // We record the last non-zero number so that we can truncate to it if needed.
-        int lastNotZero = 0;
-        int smallSize;
-        int bigSize;
-        ArrayList<Integer> smallList;
-        ArrayList<Integer> bigList;
-        if(size < rhSize || (size == rhSize && numbers.get(size - 1) <= rh.numbers.get(size - 1))) {
-            mustInverse = true;
-            smallSize = size;
-            smallList = numbers;
-            bigSize = rhSize;
-            bigList = rh.numbers;
-        } else {
-            smallSize = rhSize;
-            smallList = rh.numbers;
-            bigSize = size;
-            bigList = numbers;
-        }
-        // 1000 - 2000
-        // -(2000 - 1000)
-        for (int i = 0; i < smallSize; i++) {
-            int part = bigList.get(i) - smallList.get(i) - (carry ? 1 : 0);
-            if(part < 0) {
-                carry = true;
-                part += 10;
-                lastNotZero = i;
-            } else {
-                if(part != 0)
-                    lastNotZero = i;
-                carry = false;
-            }
+		// If there is still a carry left over after both number parts run out,
+		// it gets added to the end. (95 + 20 = >1<25)
+		if(carry)
+			added.add(1);
+		return new BigNumber(added, negative).truncateZeros();
+	}
 
-            subtracted.add(part);
-        }
-        for(int i = smallSize; i < bigSize; i++) {
-            int part = bigList.get(i) - (carry ? 1 : 0);
-            if(part < 0) {
-                carry = true;
-                part += 10;
-                lastNotZero = i;
-                subtracted.add(part);
-            } else {
-                if(part != 0)
-                    lastNotZero = i;
-                subtracted.addAll(bigList.subList(i, bigSize));
-                break;
-            }
-        }
+	/**
+	 * Subtracts rh and this BigNumber.
+	 * @param rh The BigNumber subtracted from this BigNumber.
+	 * @return A new BigNumber that's the result of subtracting rh from this BigNumber.
+	 */
+	public BigNumber subtract(BigNumber rh) {
+		// If A and B don't share a sign (A - -B or -A - B) you can do addition
+		// instead and just invert the right hand argument (A - -B -> A + B or
+		// -A - B -> -A + -B). This means that subtraction can be simplified, only
+		// handling cases where A and B share a sign and therefor decrease in magnitude.
+		if(negative ^ rh.negative)
+			return add(rh.inverse());
 
-        subtracted = new ArrayList<>(subtracted.subList(0, lastNotZero + 1));
-        return new BigNumber(subtracted, negative ^ mustInverse);
-    }
+		ArrayList<Integer> subtracted = new ArrayList<>();
+		int size = numbers.size();
+		int rhSize = rh.numbers.size();
 
-    public BigNumber multiply(BigNumber rh) {
-        return null;
-    }
+		// Order for subtraction matters, so if it's easier to subtract the right from the left
+		// (such as when the right number is larger) we need to invert everything, including the
+		// result after. This is because `a - b = -(b - a)`.
+		boolean mustInvert = size < rhSize || (size == rhSize && numbers.get(size - 1) <= rh.numbers.get(size - 1));
 
-    public BigNumber divide(BigNumber rh) {
-        return null;
-    }
+		int smallSize = mustInvert ? size : rhSize;
+		int bigSize = mustInvert ? rhSize : size;
 
-    public BigNumber getQuotient() {
-        return null;
-    }
+		ArrayList<Integer> smallList = mustInvert ? numbers : rh.numbers;
+		ArrayList<Integer> bigList = mustInvert ? rh.numbers : numbers;
 
-    public BigNumber getMod() {
-        return null;
-    }
+		// The first loop subtracts the smaller number parts from the bigger number parts,
+		// carrying as needed until the smaller one runs out. If there is still a carry,
+		// after the first loop ends, the second loop continues with just the larger number
+		// until there is no longer a carry or the bigger number parts run out.
+		boolean carry = false;
+		// First loop
+		for (int i = 0; i < smallSize; i++) {
+			int part = bigList.get(i) - smallList.get(i) - (carry ? 1 : 0);
+			if(part < 0) {
+				carry = true;
+				part += 10;
+			} else {
+				carry = false;
+			}
 
-    public BigNumber inverse() {
-        return new BigNumber(numbers, !negative);
-    }
+			subtracted.add(part);
+		}
+		// Second loop
+		for(int i = smallSize; i < bigSize; i++) {
+			int part = bigList.get(i) - (carry ? 1 : 0);
+			if(part < 0) {
+				carry = true;
+				part += 10;
+				subtracted.add(part);
+			} else {
+				subtracted.add(part);
+				subtracted.addAll(bigList.subList(i + 1, bigSize));
+				break;
+			}
+		}
+		// End loops
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(negative, numbers);
-    }
+		return new BigNumber(subtracted, negative ^ mustInvert).truncateZeros();
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder(numbers.size() + (negative ? 1 : 0));
-        if(negative)
-            builder.append('-');
-        for(int i = 0; i < numbers.size(); i++) {
-            builder.append(numbers.get(numbers.size() - 1 - i));
-        }
-        return builder.toString();
-    }
+	public BigNumber multiply(BigNumber rh) {
+		return null;
+	}
+
+	public BigNumber divide(BigNumber rh) {
+		return null;
+	}
+
+	public BigNumber getQuotient() {
+		return null;
+	}
+
+	public BigNumber getMod() {
+		return null;
+	}
+
+	public BigNumber inverse() {
+		return new BigNumber(numbers, !negative);
+	}
+
+	private BigNumber truncateZeros() {
+		int size = numbers.size();
+		for(int i = 1; i < size; i++) {
+			if(numbers.get(size - i) != 0)
+				break;
+			numbers.remove(size - i);
+		}
+		return this;
+	}
+
+	private boolean isZero() {
+		return numbers.size() == 1 && numbers.get(0) == 0;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(negative, numbers);
+	}
+
+	@Override
+	public String toString() {
+		if(isZero())
+			return "0";
+
+		StringBuilder builder = new StringBuilder(numbers.size() + (negative ? 1 : 0));
+		if(negative)
+			builder.append('-');
+		for(int i = 0; i < numbers.size(); i++) {
+			builder.append(numbers.get(numbers.size() - 1 - i));
+		}
+		return builder.toString();
+	}
 }
---
+++ src/UnitTests.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class UnitTests {
+	public static void main (String[] args) {
+
+		List<String> checks = Arrays.asList(
+			//	A,		B,		A+B,	A-B,	B-A
+				"1000",	"1",	"1001",	"999",	"-999",
+				"0",	"0",	"0",	"0",	"0",
+				"999",	"1",	"1000",	"998",	"-998",
+				"-1",	"1",	"0",	"-2",	"2",
+				"-999", "-1",	"-1000", "-998", "998",
+				"399513", "419143", "818656", "-19630", "19630",
+				"1111", "2", "1113", "1109", "-1109"
+		);
+
+		boolean totalPass = true;
+		for(int i = 0; i < checks.size(); i += 5) {
+			boolean passed = true;
+			System.out.printf("Test %s\n", i / 5);
+			BigNumber A = new BigNumber(checks.get(i));
+			BigNumber B = new BigNumber(checks.get(i + 1));
+
+			passed &= DoTest(A, B, () -> A.add(B), '+', checks.get(i + 2));
+			passed &= DoTest(B, A, () -> B.add(A), '+', checks.get(i + 2));
+			passed &= DoTest(A, B, () -> A.subtract(B), '-', checks.get(i + 3));
+			passed &= DoTest(B, A, () -> B.subtract(A), '-', checks.get(i + 4));
+			System.out.printf("%s\n\n", passed ? "Passed" : "Failed");
+			totalPass &= passed;
+		}
+		System.out.printf("Overall: %s", totalPass ? "Passed" : "Failed");
+	}
+
+	public static boolean DoTest(BigNumber A, BigNumber B, Supplier<BigNumber> test, char operator, String expected) {
+		String result = test.get().toString();
+		if(result.equals(expected)) {
+			System.out.printf("Success: %s (A) %c %s (B) = %s == %s\n", A, operator, B, result, expected);
+			return true;
+		} else {
+			System.out.printf("Failure: %s (A) %c %s (B) = %s != %s\n", A, operator, B, result, expected);
+			return false;
+		}
+	}
+}
